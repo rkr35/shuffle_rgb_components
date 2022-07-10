22a2d0ad865353bc8d723a717407a48fe54b0fdd
rkr35 <rkr35@dragons.drexel.edu>
2022-07-10 11:08:52 -0400

    Set up logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use log::info;
+
+fn main() -> Result<()> {
+    init_logger()?;
+    info!("test");
+    log::logger().flush();
+    Ok(())
+}
+
+fn init_logger() -> Result<()> {
+    use simplelog::{
+        ColorChoice, CombinedLogger, ConfigBuilder, LevelFilter, TermLogger, TerminalMode,
+        WriteLogger,
+    };
+    use std::fs::File;
+    use std::io::BufWriter;
+
+    let file = File::options().append(true).create(true).open("run.log")?;
+    let file = BufWriter::new(file);
+
+    let level = LevelFilter::Debug;
+
+    if let Ok(config) = ConfigBuilder::new()
+        .set_time_format_rfc3339()
+        .set_time_offset_to_local()
+    {
+        CombinedLogger::init(vec![
+            TermLogger::new(
+                level,
+                config.build(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            ),
+            WriteLogger::new(level, config.build(), file),
+        ])?;
+
+        Ok(())
+    } else {
+        Err(anyhow!("Unable to determine local time offset."))
+    }
 }
